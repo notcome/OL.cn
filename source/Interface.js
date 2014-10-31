6ce454f990cbfce61adf6a1d65dd6d9a710c1971
刘闽晟 <notcome@me.com>
2014-11-01 01:54:56 +0800

    simplify require hells

---
+++ source/Interface.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports = {
+  'foundation': {
+    '_': require('underscore'),
+  },
+  'io': {
+    'fs': require('fs'),
+    'http': require('http')
+  },
+  'async': {
+    'async': require('async'),
+    'suspend': require('suspend'),
+    'resume': require('suspend').resume
+  },
+  'parser': {
+    'yaml': require('js-yaml'),
+    'jade': require('jade'),
+    'lot': require('lot-parser')
+  }
+};
