13f167e28cb5ab1a4d3824f0da37afee1a1a036f
刘闽晟 <notcome@me.com>
2014-12-16 09:31:30 +0800

    automated plugin loading system (dep inject)

--- source/index.js
+++ source/index.js
@@ -13,7 +13,7 @@ function generate (dir) {
   let {files, dirs} = dir.readdirAndSort();
 
   let isTerminal = files.reduce(
-    (previous, current) => previous || current.path.slice(-8) == 'terminal',
+    (previous, current) => previous || path.basename(current.path) == 'terminal',
     false);
 
   let reducer = (previous, current) => {
@@ -35,22 +35,12 @@ function generate (dir) {
   };
 }
 
-var initialValue = {};
-
-function reducer () {}
-var compileMetadata = require('./plugins/metadata');
-var compileLot = require('./plugins/lot');
-var compileJade = require('./plugins/jade');
-
 function compileTerminal (dir) {
   let {files} = dir.readdirAndSort();
 
   console.log('compiling', dir.path);
   
-  let context =
-  [compileMetadata, compileLot, compileJade].reduce(
-    (context, plugin) => plugin(context, files),
-    {});
+  let context = plugins.reduce((context, plugin) => plugin(context, files), {});
 
   return {
     key: path.basename(dir.path),
