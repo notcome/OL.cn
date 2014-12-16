5d52a4e4fbb04bd11482f9429ed56e4ab247398c
刘闽晟 <notcome@me.com>
2014-12-16 09:31:43 +0800

    jade plugin with template support

--- source/plugins/jade.js
+++ source/plugins/jade.js
@@ -3,17 +3,28 @@
 var _ = require('underscore');
 var jade = require('jade');
 var path = require('path');
+var fs = require('fs');
 
 function init (templates) {
   return function (context, files) {
+    console.log('Compiling jade files.');
+    let jadeFiles = files.filter(file => path.basename(file.path).slice(-5) == '.jade');
 
-  jadeFiles.forEach(jadeFile => {
-    let content = jade.renderFile(jadeFile.absPath, context);
-    jadeFile.writeTextFile(content, '.html');
-    context.content = content;
-  });
+    jadeFiles.forEach(jadeFile => {
+      let content = jade.renderFile(jadeFile.absPath, context);
 
-  return context;
+      let filename = path.basename(jadeFile.path);
+      let name = filename.slice(0, -path.extname(filename).length);
+
+      if (!templates.hasOwnProperty(name))
+        return jadeFile.writeTextFile(content, '.html');
+
+      let data = templates[name].render(_.extend({}, context, { content: content }));
+      jadeFile.writeTextFile(data, '.html');
+    });
+
+    return context;
+  }
 }
 
 module.exports = init;
